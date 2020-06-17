34f0112e5c9f428c989c5d4119b34ce218db79d0
Mikhail.Ten <MT@bober-beratung.de>
2020-06-17 14:00:26 +0200

    test endpoint added

---
+++ qbotapi/Controllers/testController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using qbotapi.Controllers.ServiceInterfaces;
+using qbotapi.Resources;
+
+namespace qbotapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Test()
+        {
+            return new DateTime().ToString();
+        }
+    }
+}
